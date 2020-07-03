a8cb5b84958e99114fd11d9fd51c77050deee366
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-07-03 20:47:17 +0200

    finish socket server

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const socket = require("socket.io");
 
 const app = express();
 
 const messages = [];
 
-app.use(express.static(path.join(__dirname, '/src')));
+app.use(express.static(path.join(__dirname, "/src")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/src/index.html"));
 });
 
-app.listen(8000, () => {
-    console.log('Server runing on port 8000');
+const server = app.listen(8000, () => {
+  console.log("Server is running on Port:", 8000);
 });
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('message', (message) => {
+        console.log('Oh, I\'ve got something from ' + socket.id);
+        messages.push(message);
+        socket.broadcast.emit('message', message);
+      });
+    socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
+    console.log('I\'ve added a listener on message event \n');
+  });
